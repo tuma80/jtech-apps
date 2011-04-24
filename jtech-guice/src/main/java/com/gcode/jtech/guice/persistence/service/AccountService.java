1308703d94532efeb2b7b06d037e22cdd0dcef0e
jitendra.takalkar <jitendra.takalkar@fe2405ce-f6fa-5f27-1e81-a49ecdd25a24>
2011-04-24 17:24:06 +0000

    removed the project.

--- jtech-guice/src/main/java/com/gcode/jtech/guice/persistence/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.gcode.jtech.guice.persistence.service;
-
-import com.gcode.jtech.guice.persistence.model.Account;
-
-public interface AccountService extends PersistenceService<Account,String> {
-	
-	Account findByAccountNumber(String accountNumber);
-
-}
--- jtech-guice/src/main/java/com/gcode/jtech/guice/persistence/service/impl/HAbstractService.java
+++
@@ -1,5 +0,0 @@
-package com.gcode.jtech.guice.persistence.service.impl;
-
-public abstract class HAbstractService {
-
-}
