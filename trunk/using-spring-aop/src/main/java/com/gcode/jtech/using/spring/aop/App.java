50daab16d3868857a9df3b2de91297a14d9b626a
jitendra.takalkar <jitendra.takalkar@fe2405ce-f6fa-5f27-1e81-a49ecdd25a24>
2011-05-01 17:37:57 +0000

    New Project for Spring-AOP.

---
+++ trunk/using-spring-aop/src/main/java/com/gcode/jtech/using/spring/aop/App.java
@@ -0,0 +1,13 @@
+package com.gcode.jtech.using.spring.aop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
